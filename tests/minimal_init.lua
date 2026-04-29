434f8181b7a03f33418bd572ce92ff82ac84af18
anakin4747r2d2 <274023134+anakin4747r2d2@users.noreply.github.com>
2026-04-29 16:26:13 +0000

    fix: add plenary to runtimepath in minimal_init.lua
    --noplugin skips pack auto-loading so plenary must be added to rtp
    explicitly for the test runner to find it.

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,4 +1,5 @@
 vim.opt.swapfile = false
 vim.opt.runtimepath:append('.')
+vim.opt.runtimepath:append('vendor/plenary.nvim')
 vim.cmd('runtime plugin/plenary.vim')
 vim.cmd('runtime plugin/frenchcards.lua')
