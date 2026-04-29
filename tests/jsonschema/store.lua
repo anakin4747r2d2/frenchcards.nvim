1c1f4d99b0556756070670bbc4d084c171bc2890
anakin4747r2d2 <274023134+anakin4747r2d2@users.noreply.github.com>
2026-04-29 21:35:34 +0000

    test: copy jsonschema validator library from ai.nvim

---
+++ tests/jsonschema/store.lua
@@ -0,0 +1,465 @@
+--[[
+Copyright (c) 2019 ShenZhen ZhiLiu Technology Co., Ltd
+Copyright (c) 2017 Julien Desgats
+
+                                Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+      "You" (or "Your") shall mean an individual or Legal Entity
+      exercising permissions granted by this License.
+
+      "Source" form shall mean the preferred form for making modifications,
+      including but not limited to software source code, documentation
+      source, and configuration files.
+
+      "Object" form shall mean any form resulting from mechanical
+      transformation or translation of a Source form, including but
+      not limited to compiled object code, generated documentation,
+      and conversions to other media types.
+
+      "Work" shall mean the work of authorship, whether in Source or
+      Object form, made available under the License, as indicated by a
+      copyright notice that is included in or attached to the work
+      (an example is provided in the Appendix below).
+
+      "Derivative Works" shall mean any work, whether in Source or Object
+      form, that is based on (or derived from) the Work and for which the
+      editorial revisions, annotations, elaborations, or other modifications
+      represent, as a whole, an original work of authorship. For the purposes
+      of this License, Derivative Works shall not include works that remain
+      separable from, or merely link (or bind by name) to the interfaces of,
+      the Work and Derivative Works thereof.
+
+      "Contribution" shall mean any work of authorship, including
+      the original version of the Work and any modifications or additions
+      to that Work or Derivative Works thereof, that is intentionally
+      submitted to Licensor for inclusion in the Work by the copyright owner
+      or by an individual or Legal Entity authorized to submit on behalf of
+      the copyright owner. For the purposes of this definition, "submitted"
+      means any form of electronic, verbal, or written communication sent
+      to the Licensor or its representatives, including but not limited to
+      communication on electronic mailing lists, source code control systems,
+      and issue tracking systems that are managed by, or on behalf of, the
+      Licensor for the purpose of discussing and improving the Work, but
+      excluding communication that is conspicuously marked or otherwise
+      designated in writing by the copyright owner as "Not a Contribution."
+
+      "Contributor" shall mean Licensor and any individual or Legal Entity
+      on behalf of whom a Contribution has been received by Licensor and
+      subsequently incorporated within the Work.
+
+   2. Grant of Copyright License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      copyright license to reproduce, prepare Derivative Works of,
+      publicly display, publicly perform, sublicense, and distribute the
+      Work and such Derivative Works in Source or Object form.
+
+   3. Grant of Patent License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      (except as stated in this section) patent license to make, have made,
+      use, offer to sell, sell, import, and otherwise transfer the Work,
+      where such license applies only to those patent claims licensable
+      by such Contributor that are necessarily infringed by their
+      Contribution(s) alone or by combination of their Contribution(s)
+      with the Work to which such Contribution(s) was submitted. If You
+      institute patent litigation against any entity (including a
+      cross-claim or counterclaim in a lawsuit) alleging that the Work
+      or a Contribution incorporated within the Work constitutes direct
+      or contributory patent infringement, then any patent licenses
+      granted to You under this License for that Work shall terminate
+      as of the date such litigation is filed.
+
+   4. Redistribution. You may reproduce and distribute copies of the
+      Work or Derivative Works thereof in any medium, with or without
+      modifications, and in Source or Object form, provided that You
+      meet the following conditions:
+
+      (a) You must give any other recipients of the Work or
+          Derivative Works a copy of this License; and
+
+      (b) You must cause any modified files to carry prominent notices
+          stating that You changed the files; and
+
+      (c) You must retain, in the Source form of any Derivative Works
+          that You distribute, all copyright, patent, trademark, and
+          attribution notices from the Source form of the Work,
+          excluding those notices that do not pertain to any part of
+          the Derivative Works; and
+
+      (d) If the Work includes a "NOTICE" text file as part of its
+          distribution, then any Derivative Works that You distribute must
+          include a readable copy of the attribution notices contained
+          within such NOTICE file, excluding those notices that do not
+          pertain to any part of the Derivative Works, in at least one
+          of the following places: within a NOTICE text file distributed
+          as part of the Derivative Works; within the Source form or
+          documentation, if provided along with the Derivative Works; or,
+          within a display generated by the Derivative Works, if and
+          wherever such third-party notices normally appear. The contents
+          of the NOTICE file are for informational purposes only and
+          do not modify the License. You may add Your own attribution
+          notices within Derivative Works that You distribute, alongside
+          or as an addendum to the NOTICE text from the Work, provided
+          that such additional attribution notices cannot be construed
+          as modifying the License.
+
+      You may add Your own copyright statement to Your modifications and
+      may provide additional or different license terms and conditions
+      for use, reproduction, or distribution of Your modifications, or
+      for any such Derivative Works as a whole, provided Your use,
+      reproduction, and distribution of the Work otherwise complies with
+      the conditions stated in this License.
+
+   5. Submission of Contributions. Unless You explicitly state otherwise,
+      any Contribution intentionally submitted for inclusion in the Work
+      by You to the Licensor shall be under the terms and conditions of
+      this License, without any additional terms or conditions.
+      Notwithstanding the above, nothing herein shall supersede or modify
+      the terms of any separate license agreement you may have executed
+      with Licensor regarding such Contributions.
+
+   6. Trademarks. This License does not grant permission to use the trade
+      names, trademarks, service marks, or product names of the Licensor,
+      except as required for reasonable and customary use in describing the
+      origin of the Work and reproducing the content of the NOTICE file.
+
+   7. Disclaimer of Warranty. Unless required by applicable law or
+      agreed to in writing, Licensor provides the Work (and each
+      Contributor provides its Contributions) on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+      implied, including, without limitation, any warranties or conditions
+      of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+      PARTICULAR PURPOSE. You are solely responsible for determining the
+      appropriateness of using or redistributing the Work and assume any
+      risks associated with Your exercise of permissions under this License.
+
+   8. Limitation of Liability. In no event and under no legal theory,
+      whether in tort (including negligence), contract, or otherwise,
+      unless required by applicable law (such as deliberate and grossly
+      negligent acts) or agreed to in writing, shall any Contributor be
+      liable to You for damages, including any direct, indirect, special,
+      incidental, or consequential damages of any character arising as a
+      result of this License or out of the use or inability to use the
+      Work (including but not limited to damages for loss of goodwill,
+      work stoppage, computer failure or malfunction, or any and all
+      other commercial damages or losses), even if such Contributor
+      has been advised of the possibility of such damages.
+
+   9. Accepting Warranty or Additional Liability. While redistributing
+      the Work or Derivative Works thereof, You may choose to offer,
+      and charge a fee for, acceptance of support, warranty, indemnity,
+      or other liability obligations and/or rights consistent with this
+      License. However, in accepting such obligations, You may act only
+      on Your own behalf and on Your sole responsibility, not on behalf
+      of any other Contributor, and only if You agree to indemnify,
+      defend, and hold each Contributor harmless for any liability
+      incurred by, or claims asserted against, such Contributor by reason
+      of your accepting any such warranty or additional liability.
+
+   END OF TERMS AND CONDITIONS
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright [yyyy] [name of copyright owner]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+--]]
+
+-- This module is a store for all schemas unused in a code context.
+-- It is meant to deal with the id and $ref madness that JSON schema authors
+-- managed to put together. Resolving JSON references involves full URI
+-- parsing, absolute/relative URLs, scope management, id aliases, multipass
+-- parsing (as you have to walk the document a first time to discover all ids)
+-- and other niceties.
+--
+-- Don't try to find any logic in this code, there isn't: this is just an
+-- implementation of [1] which is foreign to the concept of *logic*.
+--
+-- [1] http://json-schema.org/latest/json-schema-core.html#rfc.section.8
+
+-- I gave up (for now) on doing a stripped down URI parser only for JSON schema
+-- needs
+local url = require 'tests.net.url'
+local schar = string.char
+
+-- the net.url is kinda weird when some uri parts are missing (sometimes it is
+-- nil, sometimes it is an empty string)
+local function noe(s) return s == nil or s == '' end
+
+-- fetching and parsing external schemas requires a lot of dependencies, and
+-- depends a lot on the application ecosystem (e.g. piping curl,  LuaSocket,
+-- cqueues, ...). Moreover, most sane schemas are self contained, so it is not
+-- even useful.
+-- So it is up to the user to provide a resolver if it's really needed
+local function default_resolver(uri)
+  error('an external resolver is required to fetch ' .. uri)
+end
+
+local function percent_unescape(x)
+  return schar(tonumber(x, 16))
+end
+local tilde_unescape = { ['~0']='~', ['~1']='/' }
+local function urlunescape(fragment)
+  return fragment:gsub('%%(%x%x)', percent_unescape):gsub('~[01]', tilde_unescape)
+end
+
+-- attempt to translate a URI fragment part to a valid table index:
+-- * if the part can be converted to number, that number+1 is returned to
+--   compensate with Lua 1-based indices
+-- * otherwise, the part is returned URL-escaped
+local function decodepart(part)
+  local n = tonumber(part)
+  return n and (n+1) or urlunescape(part)
+end
+
+
+-- a reference points to a particular node of a particular schema in the store
+local ref_mt = {}
+ref_mt.__index = ref_mt
+
+function ref_mt:child(items)
+  if not (items and items[1]) then return self end
+  local schema = self:resolve()
+  for _, node in ipairs(items) do
+    if schema[decodepart(node)] ~= nil then
+      schema = schema[decodepart(node)]
+    else
+      error("failed to find schema by node")
+    end
+  end
+  return setmetatable({ store=self.store, schema=schema }, ref_mt)
+end
+
+function ref_mt:resolve()
+  local schema = self.schema
+
+  if type(schema) ~= "table" then
+    return schema
+  end
+
+  -- resolve references
+  while schema['$ref'] do
+    -- ok, this is a ref, but what kind of ref?!?
+    local ctx = self.store:ctx(schema)
+    local ref = url.parse(ctx.base.id):resolve(schema['$ref'])
+    local fragment = ref.fragment
+
+    -- get the target schema
+    ref.fragment = nil
+    schema = self.store:fetch(tostring(ref:normalize()))
+
+    -- no fragment? just retrun the root
+    if not fragment then
+      return schema
+    end
+
+    -- maybe the fragment is a id alias
+    local by_id = self.store:ctx(ctx.base).map[fragment]
+    if by_id then
+      schema = by_id
+    else
+      -- maybe not after all, walk the schema
+      -- TODO: notrmalize path (if there is people mean enough to put '.' or
+      -- '..' components)
+      for part in fragment:gmatch('[^/]+') do
+        part = decodepart(part)
+        local new = schema[part]
+        -- TODO: add test case
+        if new == nil then
+          error(string.format('reference not found: %s#%s (at %q)',
+                              ref, fragment, part))
+        end
+        schema = new
+      end
+    end
+
+    if type(schema) ~= "table" then
+      return schema
+    end
+  end
+
+  return schema
+end
+
+
+-- a store manage all currently required schemas
+-- it is not exposed directly
+local store_mt = {}
+store_mt.__index = store_mt
+
+function store_mt:ref(schema)
+  return setmetatable({
+    store = self,
+    schema = schema,
+  }, ref_mt)
+end
+
+-- store of additional metadata by schema table part, this is to avoid
+-- modifying schema tables themselves. For now, we have
+--
+-- * `base`: refers to the base schema (e.g. for a nested subschema to find
+--   its parent schema
+-- * `map`: only for "root" schemas, maps indetifiers to subschemas
+function store_mt:ctx(t)
+  local c = self.ctx_store[t]
+  if not c then
+    c = {}
+    self.ctx_store[t] = c
+  end
+  return c
+end
+
+function store_mt:fetch(uri)
+  local schema = self.schemas[uri]
+  if schema then return schema end
+
+  -- schema not yet known
+  schema = self.resolver(uri)
+  if not schema then
+    error('faild to fetch schema for: ' .. uri)
+  end
+  if not schema.id then
+    schema.id = uri
+  end
+  self:insert(schema)
+  return schema
+end
+
+
+-- functions used to walk a schema
+local function is_schema(path)
+  local n = #path
+  local parent, grandparent = path[n], path[n-1]
+
+  return n == 0 or -- root node
+     parent == 'additionalItems' or
+     parent == 'additionalProperties' or
+     parent == 'items' or
+     parent == 'not' or
+     (type(parent) == 'number' and (
+        grandparent == 'items' or
+        grandparent == 'allOf' or
+        grandparent == 'anyOf' or
+        grandparent == 'oneOf'
+     )) or
+     grandparent == 'properties' or
+     grandparent == 'patternProperties' or
+     grandparent == 'definitions' or
+     grandparent == 'dependencies'
+end
+
+function store_mt:insert(schema)
+  local id = url.parse(assert(schema.id, 'id is required'))
+  assert(noe(id.fragment), 'schema ids should not have fragments')
+  schema.id = tostring(id:normalize())
+  self.schemas[schema.id] = schema
+  local base_id = id
+
+  -- walk the schema to collect the ids and populate the base field in context
+  local map = {}
+
+  local function walk(s, p)
+    -- handle '$id' keyword
+    if s['$id'] then
+        local u = url.parse(s['$id'])
+        if u.schema ~= nil or u.fragment == nil then
+            error("Only location independent id is supported. Unsupported $id: " .. s['$id'])
+        end
+
+        map[u.fragment] = self:ref(s).schema
+    end
+
+    local id = s.id
+    if id and s ~= schema and is_schema(p) then
+      -- there is an id, but it is not over: we have 2 different cases (!)
+      --  1. the id is a fragment: it is some kind of an internal alias
+      --  2. the id is an url (relative or absolute): resolve it using the
+      --     current base and use that as a new base.
+      if id:sub(1,1) == '#' then
+        -- fragment (case 1)
+        map[id.fragment] = self:ref(s)
+      else
+        -- relative url (case 2)
+        -- FIXME: I'm sure it's broken because resolution scopes could be
+        -- nested... but at the same time, who the hell would do this and it
+        -- passes the tests so ¯\_(ツ)_/¯
+        local resolved = base_id:resolve(id)
+        assert(noe(resolved.fragment), 'fragment in relative id')
+        s.id = tostring(resolved:normalize())
+        return self:insert(s)
+      end
+    end
+
+    self:ctx(s).base = schema
+    for k, v in pairs(s) do
+      if type(v) == 'table' and
+        (type(k) == 'number' or (
+          k ~= 'enum' and
+          k:sub(1,1) ~= '_'
+        ))
+      then
+        table.insert(p, k)
+        walk(v, p)
+        table.remove(p)
+      end
+    end
+  end
+  walk(schema, {})
+  self:ctx(schema).map = map
+  return self:ref(schema)
+end
+
+local function new(schema, resolver)
+  local self = setmetatable({
+    ctx_store = {}, -- used to store metadata about schema parts
+    schemas = {},
+    resolver = resolver or default_resolver,
+  }, store_mt)
+
+  schema.id = schema.id or 'root:'
+  return self:insert(schema)
+end
+
+return {
+  new = new,
+}
